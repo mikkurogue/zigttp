766202d412fffaa0a804429701f25df78ecd31b1
mikku@arch <michael.lindemans@outlook.com>
2025-01-05 16:31:25 +0100

    add a book of todos for serving from a folder
    also remove zig cache cause its not needed.
    
    The todos are there more to let me think about the architecture i want
    to use for this. I am a massive fan of XAMPP and how that worked, so I
    want to emulate that but in zig with some of my own constraints (like
    always defaulting to an index.html etc)
    
    this is basically nginx and xampp but for total dimwits like me

---
+++ src/fs/serve.zig
@@ -0,0 +1,25 @@
+/// TODO:
+/// serve files from a certain directory as html, js and css to the browser.
+/// Add support for php too fuck it we ball. laravel devs drive lambos
+/// Make sure it can also serve slop like React or Next dist folders
+/// Think LAMPP or XAMPP but written by a total halfwit
+///
+/// NOTE: Proposed structure of serving:
+/// www/index.html -> this will always take precedence over anything else.
+/// www/my-website/index.html -> explicitly requires user to navigate to url.com/my-website
+/// www/dist/index.html -> for instance if using a bundled output from react, next, angular or whatever
+///
+/// NOTE: Proposed configuration for the tool itself
+/// - Add a "default path" to always serve from, default config should mean this is just www/index.html
+/// - Add a "fallback path" in case the "default path" is unavailable - this in reality wont ever actually do anything
+/// but it should make things a bit easier to comprehend or have something for when ftp rollouts are happening
+/// (we dont do docker here - we do rawdogging ftp:// to the vps - devops is literally overrated and expensive
+/// for no reason btw)
+/// - Configure the port(s) allowed. However always default to 8080 for http and 443 for https.
+///  On a real instance this is probably preferred.
+///
+/// NOTE: Also figure out if we want the config to be a file, or embedded into the binary with changes allowed.
+///
+/// NOTE: We only target linux systems, because who installs windows nowadays on a vps like come on man
+/// get over yourself. Stop using windows its literal spyware
+const std = @import("std");
